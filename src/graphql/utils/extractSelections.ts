66064d35b3f73df26540da3ff28524499f6ee55c
Herbert Tamayo <hftamayo@gmail.com>
2024-07-12 09:54:50 -0600

    extractSelection module to support data extract operations

--- src/graphql/utils/extractSelections.ts
+++ src/graphql/utils/extractSelections.ts
@@ -1,2 +1,18 @@
 import { GraphQLResolveInfo } from "graphql";
 
+const getSelections = (info: GraphQLResolveInfo) => {
+  const selections = info.fieldNodes[0].selectionSet?.selections || [];
+  return selections;
+};
+
+export const extractSelection = (info: GraphQLResolveInfo) => {
+  const selections = getSelections(info);
+  if (!selections.length) return [];
+
+  return selections.reduce<string[]>((initialValue, selection) => {
+    if (selection.kind === "Field") {
+      return [...initialValue, selection.name.value];
+    }
+    return initialValue;
+  }, []);
+};
