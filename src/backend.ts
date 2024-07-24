93c8b07f2bbd06ae6d73f3170eb9d53f71f47b68
Herbert Tamayo <hftamayo@gmail.com>
2024-07-24 12:42:15 -0600

    removing express and cors from the project

--- src/backend.ts
+++ src/backend.ts
@@ -1,38 +1,36 @@
-import express, {Application} from 'express';
-import { ApolloServer, gql } from 'apollo-server-express';
-import cors from 'cors';
-import cookieParser from "cookie-parser";
-import fs from 'fs';
-import path from 'path';
-import { dbConnection, setCorsEnviro } from "./api/config/setup";
+import { ApolloServer, gql } from "apollo-server";
+import fs from "fs";
+import path from "path";
+import { dbConnection } from "./api/config/setup";
 import { port, mode } from "./api/config/envvars";
-import {todoResolver} from './api/graphql/resolvers/todo.resolver';
+import { todoResolver } from "./api/graphql/resolvers/todo.resolver";
 
-const typeDefs = gql`${fs.readFileSync(path.join(__dirname, 'api/graphql/typeDefs/todo.graphql'), 'utf8')}`;
-const app: Application = express();
+const typeDefs = gql`
+  ${fs.readFileSync(
+    path.join(__dirname, "api/graphql/typeDefs/todo.graphql"),
+    "utf8"
+  )}
+`;
 
-async function startBackEnd(){
-    try{
-        await dbConnection();
-        app.use(cors(setCorsEnviro));
-        app.use(express.json());
-        app.use(express.urlencoded({ extended: true }));
-        app.use(cookieParser());
+async function startBackEnd() {
+  try {
+    await dbConnection();
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers: [todoResolver],
+      context: ({ req, res }) => ({ req, res }),
+    });
 
-        const server = new ApolloServer({
-            typeDefs,
-            resolvers: [todoResolver],
-            context: ({ req, res }) => ({ req, res }),
-        });
-
-        server.applyMiddleware({ app, path: "/graphql" });
-
-        app.listen(port, () => {
-            console.log(`Server is running on port ${port} in ${mode} mode`);
-            console.log(`GraphQL server ready at http://localhost:${port}${server.graphqlPath}`);
-        });
-    } catch (error) {
-        console.error("Error starting GraphQLtodo backend: ", error);
-        process.exit(1);
-    }
+    server.listen(port, () => {
+      console.log(`Server is running on port ${port} in ${mode} mode`);
+      console.log(
+        `GraphQL server ready at http://localhost:${port}${server.graphqlPath}`
+      );
+    });
+  } catch (error) {
+    console.error("Error starting GraphQLtodo backend: ", error);
+    process.exit(1);
+  }
 }
+
+export { startBackEnd };
