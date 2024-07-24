555dfb9792fa4c23cdfa90df23f4b9313291f504
Herbert Tamayo <hftamayo@gmail.com>
2024-07-24 12:43:11 -0600

    removing cors enable function

--- src/api/config/setup.ts
+++ src/api/config/setup.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import { backend, whitelist_frontend, mode } from "./envvars";
+import { backend, mode } from "./envvars";
 
 const dbConnection = async () => {
   try {
@@ -17,41 +17,5 @@ const dbConnection = async () => {
   }
 };
 
-const setCorsEnviro = {
-    origin: (
-      origin: string | undefined,
-      callback: (error: Error | null, allow?: boolean) => void
-    ) => {
-      console.log(`CORS requested from origin: ${origin}`);
-      if (whitelist_frontend.indexOf(origin || "") !== -1 || !origin) {
-        console.log(`CORS requested from origin: ${origin} granted`);
-        callback(null, true);
-      } else {
-        callback(
-          new Error(`CORS requested from origin: ${origin} denied`),
-          false
-        );
-      }
-    },
-    credentials: true,
-    optionsSuccessStatus: 200,
-    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
-    allowedHeaders: [
-      "Content-Type",
-      "Access-Control-Allow-Headers",
-      "Access-Control-Allow-Origin",
-      "Access-Control-Allow-Methods",
-      "Access-Control-Allow-Credentials",
-      "Origin",
-      "withCredentials",
-      "X-Requested-With",
-      "Content-Type",
-      "Accept",
-      "Authorization",
-      "X-HTTP-Method-Override",
-      "Set-Cookie",
-      "Cookie",
-      "Request",
-    ],
-  };
-  export { dbConnection, setCorsEnviro };
+
+  export { dbConnection };
